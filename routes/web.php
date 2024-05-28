8466afee730db2c045cfbe9c3d09c347bed2749b
imronmaruf <imronfajar0902@gmail.com>
2024-05-28 17:12:03 +0700

    templating, user-management, alert

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\UserAdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/** for side bar menu active */
+function set_active($route)
+{
+    if (is_array($route)) {
+        return in_array(Request::path(), $route) ? 'active' : '';
+    }
+    return Request::path() == $route ? 'active' : '';
+}
+
+
 Route::get('/', function () {
     return view('welcome');
+});
+Route::resource('/dashboard', DashboardController::class)->only([
+    'index'
+]);
+
+Route::group(['prefix' => 'data-user'], function () {
+    Route::get('/', [UserAdminController::class, 'index'])->name('data-user.index');
+    Route::post('/store', [UserAdminController::class, 'store'])->name('data-user.store');
+    Route::delete('/destroy/{id}', [UserAdminController::class, 'destroy'])->name('data-user.destroy');
+    Route::get('/edit/{id}', [UserAdminController::class, 'edit'])->name('data-user.edit');
+    Route::put('/update/{id}', [UserAdminController::class, 'update'])->name('data-user.update');
 });
